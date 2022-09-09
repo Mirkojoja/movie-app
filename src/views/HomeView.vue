9ca771a09be029a9829dbb89a4750437d6bf7632
Mirkojoja <mirkojojovic90@gmail.com>
2022-09-09 10:33:19 +0200

    Add movies from api and display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,6 +27,11 @@
         <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Search
     </button>
 </form>
+
+    <div class="">
+      
+    </div>
+
     </div>
   <h3 class="text-center">Welcome to Movies tv</h3>
        <p class="text-center justify-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic optio autem repellendus excepturi, iure impedit corporis expedita iste, deleniti est aliquam! Omnis laudantium tenetur voluptatem cum libero vero rerum voluptas.</p>
