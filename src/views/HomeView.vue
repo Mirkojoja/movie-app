b94a7b76c3f4d1282d0a78e3c7235acc97df3a69
Slobodan <sloba_7@hotmail.com>
2022-09-13 21:50:30 +0200

    update welcome page responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,8 @@
       <div
         class="h-[200px] cover text-white px-6 py-2 bg-[url('https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')]">
         <div class="flex felx-col justify-between opacity-95 mt-5">
-          <h1 class="text-[1.2rem] font-bold text-zinc-900 bg-white py-2 px-4 border border-zinc-500 rounded-lg">Hello {{name}},
+          <h1 class="text-[1.2rem] font-bold text-zinc-900 bg-white py-2 px-4 border border-zinc-500 rounded-lg">Hello
+            {{name}},
             welcome to Movies<span class="text-red-600 font-bold">TV</span></h1>
           <button v-on:click="logout()"
             class="text-[1.2rem] font-bold bg-zinc-900 py-2 px-4 border border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white ">Logout</button>
@@ -16,19 +17,19 @@
 
         <!-- Search bar -->
         <div class="flex mt-14">
-            <div class="relative text-black">
-              <div class="flex absolute inset-y-0  items-center pl-3 pointer-events-none">
-                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"></path>
-                </svg>
-              </div>
-              <input type="text" id="voice-search"
-                class="lg:w-[550px] pl-10 p-2.5 w-full text-[1.2rem]  bg-white py-2 px-4 border border-zinc-500 rounded-lg"
-                placeholder="Search Movies" required @keyup.enter="submit" v-model="query">
+          <div class="relative text-black">
+            <div class="flex absolute inset-y-0  items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"></path>
+              </svg>
             </div>
+            <input type="text" id="voice-search"
+              class="lg:w-[550px] pl-10 p-2.5 w-full text-[1.2rem]  bg-white py-2 px-4 border border-zinc-500 rounded-lg"
+              placeholder="Search Movies" required @keyup.enter="submit" v-model="query">
+          </div>
           <div>
             <button type="submit"
               class="ml-5 inline-flex items-center  text-[1.2rem]  bg-zinc-900 py-2 px-4 border text-white  border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white">
@@ -64,10 +65,10 @@
 
       <div class="flex-1 overflow-auto ..."></div>
       <div class="flex justify-center flex-wrap m-4">
-  
-  
+
+
         <PopularMovies />
-  
+
       </div>
     </div>
   </div>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,28 +1,30 @@
 <template>
-<div class="p-5 w-full h-screen bg-[url('https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')]">
-    <div class="bg-zinc-900 w-[800px] mx-auto border rounded-lg text-center text-white opacity-95 p-4">
-        <h1 class="text-[3rem]">Welcome to <span class="font-bold">Movies</span><span class="font-bold text-red-600">TV</span></h1>
-        <div class="m-2 px-6 py-4">
-            <h1 class="text-xl mb-4 font-bold text-white"> Please SingUp to view content</h1> 
-            <router-link to="/sign-up">
-            <button
-                class="text-[1.2rem] font-bold bg-zinc-900 py-2 px-4 border border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white ">
-                    Sign Up
-                </button>
-            </router-link>
-            <router-link to="/login">
-                <button
-                class="ml-5 text-[1.2rem] font-bold bg-zinc-900 py-2 px-4 border border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white ">Login
-                </button>
-            </router-link>
+    <div
+        class="p-5 h-screen bg-[url('https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')]">
+        <div class="bg-zinc-900 lg:w-[800px] mx-auto border rounded-lg text-center text-white opacity-95 p-4">
+            <h1 class="text-[3rem]">Welcome to <span class="font-bold">Movies</span><span
+                    class="font-bold text-red-600">TV</span></h1>
+            <div class="m-2 px-6 py-4">
+                <h1 class="text-xl mb-4 font-bold text-white"> Please SingUp to view content</h1>
+                <router-link to="/sign-up">
+                    <button
+                        class="text-[1.2rem] font-bold bg-zinc-900 py-2 px-4 border border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white ">
+                        Sign Up
+                    </button>
+                </router-link>
+                <router-link to="/login">
+                    <button
+                        class="ml-5 text-[1.2rem] font-bold bg-zinc-900 py-2 px-4 border border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white ">Login
+                    </button>
+                </router-link>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 
 <script>
-    export default {
-        name: 'Welcome'
-    }
+export default {
+    name: 'Welcome'
+}
 </script>
