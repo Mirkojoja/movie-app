09761467e008ff777b178e9ee6bb697a7a5fc254
Slobodan1991 <slobodanperisic91@gmail.com>
2022-09-13 15:27:27 +0200

    update pages login and signup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
         class="h-[200px] cover text-white px-6 py-2 bg-[url('https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')]">
         <div class="flex felx-col justify-between opacity-95 mt-5">
           <h1 class="text-[1.2rem] font-bold text-zinc-900 bg-white py-2 px-4 border border-zinc-500 rounded-lg">Hello {{name}},
-            welcome to MovieTV</h1>
+            welcome to Movies<span class="text-red-600 font-bold">TV</span></h1>
           <button v-on:click="logout()"
             class="text-[1.2rem] font-bold bg-zinc-900 py-2 px-4 border border-zinc-500 rounded-lg hover:bg-zinc-700 hover:text-white ">Logout</button>
         </div>
